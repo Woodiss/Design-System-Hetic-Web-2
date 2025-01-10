9733344b370642f944acdfa2dc6e7d88e57fb81e
Adrien Allard <allard15@hotmail.fr>
2025-01-10 14:32:33 +0100

    Update Button and Input outlines

--- taskbox/src/components/textarea/Textarea.jsx
+++ taskbox/src/components/textarea/Textarea.jsx
@@ -11,7 +11,7 @@ export const Textarea = ({ id, placeholder, label, error, defaultValue, name, is
       {label && <label className='text-regular-semi-bold' htmlFor={id}>{label}</label>}
       {error && (
         <div className="textarea-message" style={{color: "blue"}}>
-          <Icon src={errorIcon} alt={"Error Icon"} size={20}/>
+          <Icon size={'20px'} name={"errorIcon"} color={"var(--accent-danger)"} ariaLabel={"Error Icon"} title={"Error Icon"} role={"img"}/>
           <Text tag={'p'} content={error} optionalClassName={'textarea-error'}/>
         </div>
         )}
