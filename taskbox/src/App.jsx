732d60116b7ec38c56df475c97262e3333f549ef
Adrien Allard <allard15@hotmail.fr>
2025-01-10 14:07:22 +0100

    Update favicon, title on index.html

--- taskbox/src/App.jsx
+++ taskbox/src/App.jsx
@@ -20,9 +20,9 @@ function App() {
             path="/"
             element={
               <Form title="Login Form" subtitle="Please enter your credentials">
-                <Input type="email" placeholder="Your Email" label="Email" name="email" isRequired="true"/>
-                <Input type="password" placeholder="Your Password" label="Password" name="password" isRequired="true"/>
-                <LinkTag content="Forgot Password?" link="/forgot-password" />
+                <Input type="email" placeholder="Enter your email" label="Email" name="email" isRequired="true"/>
+                <Input type="password" placeholder="Enter your password" label="Password" name="password" isRequired="true"/>
+                <LinkTag content="Forgot password?" link="/forgot-password" />
                 <Button type="submit" content="Login" />
               </Form>
             }
@@ -31,7 +31,7 @@ function App() {
             path="/forgot-password"
             element={
               <Form title="Reset your password" subtitle="Please enter your details so we can send you a email to reset your password.">
-                <Input type="email" placeholder="Your email" label="Email" name="email"isRequired="true" />
+                <Input type="email" placeholder="Enter your email" label="Email" name="email"isRequired="true" />
                 <LinkTag content="Return to login" link="/" />
                 <Button type="submit" content="Send" />
               </Form>
@@ -41,11 +41,12 @@ function App() {
             path="/contact"
             element={
               <Form title="Contact support" subtitle="If you have any problem you can contact support.">
-                <Input type="firstname" placeholder="Your first name" label="First name" name="firstname" isRequired="true"/>
-                <Input type="lastname" placeholder="Your last name" label="Last name" name="lastname" isRequired="true"/>
-                <Input type="email" placeholder="Your email" label="Email" name="email" isRequired="true"/>
+                <Input type="firstname" placeholder="Enter your first name" label="First name" name="firstname" isRequired="true"/>
+                <Input type="lastname" placeholder="Enter your last name" label="Last name" name="lastname" isRequired="true"/>
+                <Input type="email" placeholder="Enter your email" label="Email" name="email" isRequired="true"/>
                 <Select label="Select a topic" name="topic" options={["General", "Billing", "Technical", "Others"]} />
-                <Textarea label="Message" placeholder="Your message" name="message" id="textarea" isRequired="true" />
+                <Textarea label="Message" placeholder="Enter your message" name="message" id="textarea" isRequired="true" />
+                <LinkTag content="Return to login" link="/" />
                 <Button type="submit" content="Send" />
               </Form>
             }
